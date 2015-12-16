395944231f20ba42984308e4d27afc7ee6cbf3e3
peter23 <i@peter23.com>
2015-12-16 12:43:49 +0300

    Improved

--- PicoDataRecord.php
+++ PicoDataRecord.php
@@ -2,9 +2,8 @@
 
 	class PicoDataRecord {
 
-		public $__fields;
-
 		public $__data;
+		public $__fields;
 
 
 		public function __construct($fields) {
@@ -13,6 +12,7 @@
 		}
 
 
+		//LOAD
 		public function rawLoad($data) {
 			foreach($this->__fields as $field_name=>&$field_data) {
 				if(isset($data[$field_name])) {
@@ -21,83 +21,173 @@
 			}
 		}
 
-
-		public function filter() {
-			$ret = array();
-			foreach($this->__fields as $field_name=>&$field_data) {
-
-				//for each field:
-
-				//converting
-				switch($field_data['type']) {
-					case 'date':
-						$this->__data[$field_name] = strtotime($this->__data[$field_name]);
-						if($this->__data[$field_name]) {
-							$this->__data[$field_name] = date('Y-m-d', $this->__data[$field_name]);
-						}
-						break;
-					case 'datetime':
-						$this->__data[$field_name] = strtotime($this->__data[$field_name]);
-						if($this->__data[$field_name]) {
-							$this->__data[$field_name] = date('Y-m-d H:i:s', $this->__data[$field_name]);
-						}
-						break;
-					case 'list':
-						if(!isset($field_data['values'][$this->__data[$field_name]])) {
-							$this->__data[$field_name] = null;
-						}
-						break;
-					case 'number':
-						$this->__data[$field_name] = trim($this->__data[$field_name]);
-						if(strlen($this->__data[$field_name])) {
-							$this->__data[$field_name] = str_replace(',', '.', $this->__data[$field_name]);
-							$this->__data[$field_name] = floatval($this->__data[$field_name]);
-						} else {
-							$this->__data[$field_name] = null;
-						}
-						break;
-				}
-
-				//filtering
-				if(isset($field_data['filter'])) {
-					foreach($field_data['filter'] as $filter) {
-						//for each filter:
-						$err = false;
-						switch($filter) {
-							case 'required':
-								if(!strlen($this->__data[$field_name])) {
-									$err = true;
-								}
-								break;
-
-							case 'trim':
-								$this->__data[$field_name] = trim($this->__data[$field_name]);
-								break;
-
-							default:
-								throw new Exception('PicoDataRecord.filter: Unknown filter "'.$filter.'"');
-								break;
-						}
-						if($err) {
-							$ret[] = array($field_name, $filter);
-						}
-					}  //foreach filter
-				}
-
-			}  //foreach field
-			return $ret;
-		}
-
-
 		public function load($data) {
 			$this->rawLoad($data);
 			return $this->filter();
 		}
 
 
+		//CONVERT AND FILTER
+		public function convertVal($val, &$field_data) {
+			//we will process any value as array value
+			if(!is_array($val)) {
+				$val = array($val);
+			}
+
+			//if it is array value and number of elements is defined, then fill all non-existent elements with null
+			if(isset($field_data['array']) && $field_data['array']) {
+				for($i = 0; $i < $field_data['array']; $i++) {
+					if(!isset($val[$i]))  $val[$i] = null;
+				}
+			}
+
+			//process value
+			switch($field_data['type']) {
+
+				case 'date':
+					foreach($val as &$val1) {
+						$val1 = strtotime(trim($val1));
+						if($val1) {
+							$val1 = date('Y-m-d', $val1);
+						}
+					}
+					break;
+
+				case 'datetime':
+					foreach($val as &$val1) {
+						$val1 = strtotime(trim($val1));
+						if($val1) {
+							$val1 = date('Y-m-d H:i:s', $val1);
+						}
+					}
+					break;
+
+				case 'list':
+					foreach($val as &$val1) {
+						if(!isset($field_data['values'][$val1])) {
+							$val1 = null;
+						}
+					}
+					break;
+
+				case 'number':
+					foreach($val as &$val1) {
+						$val1 = str_replace(array(' ', "\t", "\n", "\r"), '', $val1);
+						if(strlen($val1)) {
+							$val1 = str_replace(',', '.', $val1);
+							$val1 = floatval($val1);
+						} else {
+							$val1 = null;
+						}
+					}
+					break;
+
+				case 'text':
+					break;
+
+				default:
+					throw new Exception('PicoDataRecord.convertVal: Unknown type "'.$field_data['type'].'"');
+					break;
+
+			}
+
+			if(!isset($field_data['array'])) {
+				return $val[0];
+			} else {
+				return $val;
+			}
+		}
+
+		public function filterVal($val, $filter) {
+			//we will process any value as array value
+			if(!is_array($val)) {
+				$val = array($val);
+				$no_array = true;
+			}
+
+			switch($filter) {
+
+				case 'delete_empty':
+					//delete empty elements (for array)
+					$new_val = array();
+					foreach($val as $val_n=>$val1) {
+						if(strlen($val1)) {
+							$new_val[$val_n] = $val1;
+						}
+					}
+					$val = $new_val;
+					break;
+
+				case 'required':
+					//required at least one
+					foreach($val as &$val1) {
+						if(strlen($val1)) {
+							//go out from foreach and switch
+							break 2;
+						}
+					}
+					throw new PicoDataRecordFilterException;
+					break;
+
+				case 'required_all':
+					//required all (for array)
+					foreach($val as &$val1) {
+						if(!strlen($val1)) {
+							throw new PicoDataRecordFilterException;
+						}
+					}
+					break;
+
+				case 'trim':
+					foreach($val as &$val1) {
+						$val1 = trim($val1);
+					}
+
+					break;
+
+				default:
+					throw new Exception('PicoDataRecord.filterVal: Unknown filter "'.$filter.'"');
+					break;
+
+			}
+
+			if(isset($no_array)) {
+				return $val[0];
+			} else {
+				return $val;
+			}
+		}
+
+		public function filter() {
+			$ret = array();
+			foreach($this->__fields as $field_name=>&$field_data) {
+				$this->__data[$field_name] = $this->convertVal($this->__data[$field_name], $field_data);
+
+				if(isset($field_data['filter'])) {
+					foreach($field_data['filter'] as $filter) {
+						try {
+							$this->__data[$field_name] = $this->filterVal($this->__data[$field_name], $filter);
+						} catch(PicoDataRecordFilterException $e) {
+							$ret[] = array($field_name, $filter);
+						}
+					}
+				}
+			}
+			return $ret;
+		}
+
+
+		//OUTPUT
 		public function __get($name) {
 			if(!isset($this->__fields[$name])) {
 				throw new Exception('PicoDataRecord.__get: Undefined field "'.$name.'"');
+			}
+
+			$val = $this->__data[$name];
+			//we will process any value as array value
+			if(!is_array($val)) {
+				$val = array($val);
+				$no_array = true;
 			}
 
 			switch($this->__fields[$name]['type']) {
@@ -105,14 +195,18 @@
 				case 'datetime':
 				case 'number':
 				case 'text':
-					$ret = $this->encodeField($name, $this->__data[$name]);
+					foreach($val as &$val1) {
+						$val1 = $this->encodeField($name, $val1);
+					}
 					break;
 
 				case 'list':
-					$ret = array(
-						$this->encodeField($name, $this->__data[$name]),
-						$this->encodeField($name, $this->__fields[$name]['values'][$this->__data[$name]]),
-					);
+					foreach($val as &$val1) {
+						$val1 = array(
+							$this->encodeField($name, $val1),
+							$this->encodeField($name, isset($this->__fields[$name]['values'][$val1]) ? $this->__fields[$name]['values'][$val1] : null),
+						);
+					}
 					break;
 
 				default:
@@ -120,7 +214,11 @@
 					break;
 			}
 
-			return $ret;
+			if(isset($no_array)) {
+				return $val[0];
+			} else {
+				return $val;
+			}
 		}
 
 
@@ -136,6 +234,15 @@
 			return $this->encodeField($name, $this->__fields[$name]['values']);
 		}
 
+
+		public function encodeVal($enc_type, $value) {
+			if($enc_type == 'html') {
+				$value = htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
+			} elseif($enc_type == 'url') {
+				$value = urlencode($value);
+			}
+			return $value;
+		}
 
 		public function encodeField($name, $value) {
 			if(!isset($this->__fields[$name])) {
@@ -163,30 +270,21 @@
 				}
 			}
 
-			if(isset($this->__fields[$name]['encode'])) {
-				$enc_type = $this->__fields[$name]['encode'];
-				if(!is_array($value)) {
-					$value = $this->encodeVal($enc_type, $value);
-				} else {
-					$new_value = array();
-					foreach($value as $value_n=>$value1) {
-						$new_value[$this->encodeVal($enc_type, $value_n)] = $this->encodeVal($enc_type, $value1);
-					}
-					$value = $new_value;
+			$enc_type = isset($this->__fields[$name]['encode']) ? $this->__fields[$name]['encode'] : 'html';
+			if(!is_array($value)) {
+				$value = $this->encodeVal($enc_type, $value);
+			} else {
+				$new_value = array();
+				foreach($value as $value_n=>$value1) {
+					$new_value[$this->encodeVal($enc_type, $value_n)] = $this->encodeVal($enc_type, $value1);
 				}
+				$value = $new_value;
 			}
 
-			return $value;
-		}
-
-
-		public function encodeVal($enc_type, $value) {
-			if($enc_type == 'html') {
-				$value = htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
-			} elseif($enc_type == 'url') {
-				$value = urlencode($value);
-			}
 			return $value;
 		}
 
 	}
+
+
+	class PicoDataRecordFilterException extends Exception { }
